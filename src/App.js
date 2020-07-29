9eb2147a348eb255f5c25e64e2fa576252490734
arthurgehrke <arthurgehrke07@gmail.com>
2020-07-29 20:20:42 -0300

    feat: add global styles

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 import React from 'react';
 import { BrowserRouter } from 'react-router-dom';
 
+import GlobalStyles from './styles/GlobalStyles';
+
 import Home from './pages/home'; 
 
 const App = () => {
   return (
     <BrowserRouter>
       <Home />
+
+      <GlobalStyles />
     </BrowserRouter>
   );
 };
