3326850e8eab6c3aa675d031f26c363199e4fefa
arthurgehrke <arthurgehrke07@gmail.com>
2020-07-29 21:58:51 -0300

    feat: start footer

--- src/pages/home/index.jsx
+++ src/pages/home/index.jsx
@@ -1,10 +1,17 @@
 import React from 'react';
 
 import Header from '../../components/header';
+import Footer from '../../components/footer';
+
+import { Container } from './styles'
 
 const Home = () => { 
   return (
-    <Header />
+    <Container>
+      <Header />
+      <h1>Hello</h1>
+      <Footer />
+    </Container>
   );
 }
 
