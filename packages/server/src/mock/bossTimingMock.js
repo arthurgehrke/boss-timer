053b9075a138c844f582095ecda8b30d666caa13
arthurgehrke <arthurgehrke07@gmail.com>
2020-07-30 12:59:41 -0300

    feat: add bossTimmingMock in server package

---
+++ packages/server/src/mock/bossTimingMock.js
@@ -0,0 +1,8 @@
+module.exports = {
+  boss1: "17:00",
+  boss2: "18:00",
+  boss3: "19:00",
+  boss4: "20:00",
+  boss5: "21:00",
+  boss6: "22:00",
+}
--- packages/server/src/routes.js
+++ packages/server/src/routes.js
@@ -1,9 +1,15 @@
 const { Router } = require('express')
 
+const bossTimmingMock = require('./mock/bossTimingMock')
+
 const routes = Router()
 
 routes.get('/', (_, response) => {
   return response.status(200).json({ message: 'Hello World' })
+})
+
+routes.get('/bossTiming', (_, response) => {
+  return response.status(200).json(bossTimmingMock)
 })
 
 module.exports = routes
