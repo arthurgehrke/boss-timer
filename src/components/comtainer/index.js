999b10c63ca74aee6c689c6560de8f3cbcac6261
José Vitor <jose81073175@gmail.com>
2020-07-29 22:43:03 -0300

    edit container

--- src/components/comtainer/index.js
+++ src/components/comtainer/index.js
@@ -3,7 +3,7 @@ import styled from 'styled-components'
 export const Container = styled.div`
 	display: flex;
 	flex-direction: column;
-	text-align: center;
+	align-items: center;
 	width: 100%;
 	background-color: #103b57;
 `
